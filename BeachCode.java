494ef48e2feb7761cfc870773694991712cb2a13
Armitage <evana56852@stu.powayusd.com>
2022-10-19 14:43:04 -0700

    cleaning

--- BeachCode.java
+++ BeachCode.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class BeachCode {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("I want to surf, where should we go surfing?");
+        System.out.println("Its the fabeled summer break and you realize there is only one thing to do with your time \nSURF!! \nSo where should you go surfing?");
         String[] surfList = {
             "Pipeline",
             "The Wedge",
@@ -28,9 +28,10 @@ public class BeachCode {
             chosenSpots[i]= surfList[surfSpotNew];
             surfList[surfSpotNew] = "";
         }
-        System.out.println("The surf today is looking good in ");
-        for(String s : chosenSpots){System.out.print(s+", ");}
-        System.out.println();
+        System.out.println("You check your phone for the best spots of the day\n-----------------------------------\nThe surf today is looking good in ");
+        for(String s : chosenSpots){System.out.print(s+",\n ");}
+        System.out.println("-----------------------------------");
+        System.out.println("Which spot you going to conquer today?");
         boolean flag = false;
         String surfSpot = "";
         while(flag==false){
@@ -41,8 +42,10 @@ public class BeachCode {
                 }
             }
         }
-        System.out.println(surfSpot + " seems like fun ");
-    scan.close(); 
+        System.out.println("-----------------------------------");
+        System.out.println(surfSpot + " seems like fun, the waves there are barreling like crazy!!");
+        System.out.println("With the batlefield "+ surfSpot + "chosen, you now need your weapon of choice.\n");
+        scan.close(); 
         
     }
 }
